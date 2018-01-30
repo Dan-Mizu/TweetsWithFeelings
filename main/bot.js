cd8fc7c23db7cc263695c389c718cb358daa81be
Danny <baghdadi.danny@gmail.com>
2018-01-29 20:18:16 -0800

    finished (?)
    made this all a while ago but I am finally uploading it to github

---
+++ main/bot.js
@@ -0,0 +1,66 @@
+//Dependencies
+var config = require('./config');
+var Sentencer = require('sentencer');
+var txtgen = require('txtgen');
+var Twit = require('twit');
+var T = new Twit(config);
+
+//Variable Calling
+var tweetContent;
+var loopN = 1;
+var eFeelings = ['😀','😁','😂','🤣','😃','😄','😅','😆','😉','😊','😋','😎','😍','😘','😗','😙','☺️','🙂','🤗','🤩','🤔','🤨','😐','😑','😶','🙄','😏','😣','😥','😮','🤐','😯','😪','😫','😴','😌','😛','😜','😝','🤤','😒','😓','😔','😕','🙃','🤑','😲','☹️','🙁','😖','😞','😟','😤','😢','😭','😦','😧','😨','😩','🤯','😬','😰','😱','😳','🤪','😵','😡','😠','🤬','😷','🤒','🤕','🤢','🤮','🤧','😇','🤠','🤡','🤥','🤫','🤭','🧐','🤓','😈','👿','💀','💩'];
+
+//Randomizer Function
+function getRandomInt(min, max) {
+    min -= 1;
+    max -= 1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Bot Has Initiated
+console.log (
+    '********** TweetsWithFeelings Bot Init **********\n'
+);
+
+//Loop Function
+function startLoop(loopTime) {
+    tweetFeelings();
+    var bufferTime = loopTime;
+    var bufferAmount = 0;
+    process.stdout.write('Loop Timer: ' + bufferTime);
+    var countdownTimer = setInterval(function () {
+        readline.cursorTo(process.stdout, 0);
+        bufferTime -= 1;
+        process.stdout.write('Loop Timer: ' + bufferTime);
+        bufferAmount += 1;
+        if (bufferTime <= 0) {
+            clearInterval(countdownTimer);
+            readline.clearLine(process.stdout);
+            readline.cursorTo(process.stdout, 0);
+            console.log('Buffer Time: ' + bufferAmount + '\n\n\n');
+            loopN += 1;
+            startLoop(loopTime);
+        }
+    }, 1000);
+}
+
+//Starts the Tweet Cycle
+startLoop(10);
+
+//The Actual Tweeting Part Thing
+function tweetFeelings() {
+    tweetContent = txtgen.sentence() + '\n\n--Feeling: ' + Sentencer.make('{{ adjective }} ') + eFeelings[getRandomInt(1, eFeelings.length)];
+    T.post('statuses/update', { status: tweetContent }, tweetInfo);
+}
+
+//This tells me if the tweet sent and what it was
+function tweetInfo(err, data, response) {
+    if (err) {
+        console.log('\n >Error Tweeting.\n');
+    } else {
+        console.log('__\n >Successfully Tweeted:\n' + tweetContent + '\n__\n');
+        startTimer();
+    }
+}
+
+return tweetContent;
