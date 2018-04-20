ad9c8d139442313d3452a977514f22ab213ab7f6
Dan Ann <baghdadi.danny@gmail.com>
2018-04-20 07:51:25 -0700

    Delete config.js

--- main/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key: 'BOSCvHBK9SdHrwnpeUVVyvPTR',
-    consumer_secret: 'Jh6TutnydsNcl3d9WseHytNsRCulP7GsCy2gNEGQtqgRJSA813',
-    access_token: '955598993223905280-vq5pqLyOKTIaRfcMrYvZbTKY1mrdK86',
-    access_token_secret: 'crC3UBgkBsEXjDdxMggveMgVnjkK2YEC88DIaey5oG3P0',
-    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-}
